df7d5cf7eab95fadb0fe56dd0ea16fd17a82c7c3
Nick Nderitu <nick.nderitu@xoko.co.ke>
2020-11-26 12:57:20 +0300

    added length

--- helpers/requestsHelper.js
+++ helpers/requestsHelper.js
@@ -3,37 +3,37 @@ var mongo = require("mongodb").MongoClient;
 const { ObjectId } = require("mongodb");
 var url = "mongodb://127.0.0.1:27017/xlparser";
 
-function getAllRequests(callback){
-    try {
-        mongo.connect(url, function (err, db) {
-          if (err) {
-            resp.send({
-              status: 0,
-              message: "Error:Error occured connecting",
-              response: {},
-            });
-          }
-          var dbo = db.db("xlparser");
-          dbo
-            .collection("requests")
-            .find({ status:1 })
-            .toArray(function (err, res) {
-              if (err) {
-                //resp.send({ status: 0, message: "Error:" + res, response: {} });
-                callback([]);
-              }
-              console.log(res);
-              callback(res)
-            });
+function getAllRequests(callback) {
+  try {
+    mongo.connect(url, function (err, db) {
+      if (err) {
+        resp.send({
+          status: 0,
+          message: "Error:Error occured connecting",
+          response: {},
         });
-      } catch (err) {
-        console.log(err);
-        callback([]);
-        //resp.send({ status: 0, message: "Error:" + err, response: {} });
       }
+      var dbo = db.db("xlparser");
+      dbo
+        .collection("requests")
+        .find({ status: 1 })
+        .toArray(function (err, res) {
+          if (err) {
+            //resp.send({ status: 0, message: "Error:" + res, response: {} });
+            callback([]);
+          }
+          console.log(res);
+          callback(res);
+        });
+    });
+  } catch (err) {
+    console.log(err);
+    callback([]);
+    //resp.send({ status: 0, message: "Error:" + err, response: {} });
+  }
 }
 
-function getRequestById(_id,callback){
+function getRequestById(_id, callback) {
   console.log("_id", _id);
   try {
     mongo.connect(url, function (err, db) {
@@ -45,7 +45,9 @@ function getRequestById(_id,callback){
         .toArray(function (err, res) {
           if (err) throw err;
           console.log(res);
-          callback(res);
+          if (res.length) {
+            callback(res[0]);
+          }
         });
     });
   } catch (err) {
@@ -53,5 +55,4 @@ function getRequestById(_id,callback){
   }
 }
 
-
-module.exports = {getAllRequests,getRequestById};
+module.exports = { getAllRequests, getRequestById };
