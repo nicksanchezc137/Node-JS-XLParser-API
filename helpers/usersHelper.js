988a55da8767ec28e08e6a017da51900d22db8b4
Nick Nderitu <nick.nderitu@xoko.co.ke>
2020-11-26 13:37:20 +0300

    to string

--- helpers/usersHelper.js
+++ helpers/usersHelper.js
@@ -41,7 +41,7 @@ function getUsertById(_id,callback){
       var dbo = db.db("xlparser");
       dbo
         .collection("users")
-        .find({ _id: new ObjectId(_id) })
+        .find({ _id:  ObjectId(_id) })
         .toArray(function (err, res) {
           if (err) throw err;
           console.log(res);
